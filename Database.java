5a30ef6780a139b830229a0328ea712d65409083
justin kamp <justinkamp3505428@gmail.com>
2020-08-06 18:00:27 -0400

    Setting up hibernate for appending the database

--- Database.java
+++ Database.java
@@ -1,4 +1,40 @@
 package Main;
 
+import Main.entity.Entity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Database {
+
+    public static void dataEntry(String[] profile) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Entity.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            Entity newProfile = new Entity(
+                    profile[0],
+                    profile[1],
+                    profile[2],
+                    profile[3],
+                    profile[4],
+                    profile[5],
+                    profile[6],
+                    profile[7],
+                    profile[8],
+                    profile[9]);
+            session.beginTransaction();
+            session.save(newProfile);
+            session.getTransaction().commit();
+
+        } finally {
+            factory.close();
+
+        }
+
+    }
+
 }
