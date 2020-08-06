08e3d06416b7ab0926c3995da556c4b354e13dd6
justin kamp <justinkamp3505428@gmail.com>
2020-08-06 18:03:07 -0400

    Adding final command for main statement

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 package Main;
 
 
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // write your code here
+        DataInput.Data();
+        Log.logStatistics();
     }
 }
