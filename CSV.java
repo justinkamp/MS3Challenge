0026e301171222af41129b99ac9f37b562d921ca
justin kamp <justinkamp3505428@gmail.com>
2020-08-06 17:59:33 -0400

    Creating the CSV file

--- CSV.java
+++ CSV.java
@@ -1,4 +1,20 @@
 package Main;
 
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class CSV {
+
+    public static void invalidData(String[] profile) throws IOException {
+
+        //Iterating through the flagged profile and appending each to the Invalid CSV
+        for (String iterate: profile) {
+            Files.write(Paths.get("InvalidRecords_bad.csv"), iterate.getBytes(),StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            Files.write(Paths.get("InvalidRecords_bad.csv"), ", ".getBytes(), StandardOpenOption.APPEND);
+        }
+        Files.write(Paths.get("InvalidRecords_bad.csv"), "\n".getBytes(), StandardOpenOption.APPEND);
+    }
 }
